5d233611be053118f4b73d85ee43a4359933d4d5
Голубев Алексей <dr.freecx@gmail.com>
2015-05-10 16:37:16 +0300

    [init] flask microframework

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
+from flask import Flask, render_template
 import simulator
 import student
 
-if __name__ == '__main__':
-    """ main simulator class """
-    sm = simulator.Simulator(1)
-    """ main student class """
-    st = student.Student()
-    """ append student to simulator """
-    sm.appendObject(st, 1)
+""" main simulator class """
+sm = simulator.Simulator(1)
+""" main student class """
+st = student.Student()
+""" append student to simulator """
+sm.appendObject(st, 1)
+""" init flask """
+app = Flask(__name__)
 
-    """ start simulator """
-    sm.start()
-    test = input('---- Press enter to quit! ----\n')
-    """ stop simulator """
-    sm.stop()
+def getParamList():
+    return {
+        'mood': int(st.mood),
+        'progress': int(st.progress),
+        'satiety': int(st.satiety),
+        'finances': int(st.finances)
+    }
+
+@app.route('/cmd/<command>')
+def cmd(command):
+    if command == 'start':
+        sm.start()
+    elif command == 'stop':
+        sm.stop()
+    return render_template('index.html', param=getParamList())
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', param=getParamList())
+
+if __name__ == '__main__':
+    """ run flask """
+    app.run(debug=True)
